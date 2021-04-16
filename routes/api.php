81e6c9c0379f65659f35f8c1594c55e1eb41adef
Viktoria <hello@viktoriamf.com>
2021-04-16 15:44:20 +0200

    feat(locations): add get endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\VaccinationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +31,12 @@ Route::put('vaccinations/{id}', [VaccinationController::class, 'update']);
 
 Route::delete('vaccinations/{id}', [VaccinationController::class, 'delete']);
 
+/**
+ * users
+ */
 Route::get('users', [UserController::class, 'getAll']);
+
+/**
+ * locations
+ */
+Route::get('locations', [LocationController::class, 'getAll']);
